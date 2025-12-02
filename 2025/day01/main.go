6e0177aa0e7f31545884904460a0af89932432be
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2025-12-02 23:49:56 +0530

    day01-part2

--- 2025/day01/main.go
+++ 2025/day01/main.go
@@ -33,6 +33,35 @@ func part1(scanner *bufio.Scanner) int {
 	return ans
 }
 
+func part2(scanner *bufio.Scanner) int {
+	cur, ans := 50, 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		steps, err := strconv.Atoi(line[1:])
+		if err != nil {
+			panic(err)
+		}
+		ans += steps / 100 // crosses zero
+		steps %= 100
+
+		if line[0] == 'L' {
+			if cur != 0 && steps >= cur {
+				ans++
+			}
+			cur = (cur - steps + 100) % 100
+		} else {
+			if cur != 0 && steps >= (100-cur) {
+				ans++
+			}
+			cur = (cur + steps) % 100
+		}
+	}
+
+	return ans
+}
+
 func main() {
 	inp, err := os.Open("./input.txt")
 	if err != nil {
@@ -42,5 +71,6 @@ func main() {
 
 	scanner := bufio.NewScanner(inp)
 
-	fmt.Println(part1(scanner))
+	// fmt.Println(part1(scanner))
+	fmt.Println(part2(scanner))
 }
