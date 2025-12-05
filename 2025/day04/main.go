1f93b892e9c67b68da257fb315169c72f477826b
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2025-12-06 04:53:41 +0530

    day04-part2

--- 2025/day04/main.go
+++ 2025/day04/main.go
@@ -5,33 +5,92 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	dll "github.com/emirpasic/gods/lists/doublylinkedlist"
 )
 
-func getVal(i int, j int, graph []string) int {
-	if (0 <= i && i < len(graph)) && (0 <= j && j < len(graph[0])) {
-		if graph[i][j] == '@' {
-			return 1
-		} else {
-			return 0
-		}
-	} else {
-		return 0
-	}
+type Coord struct {
+	R int
+	C int
 }
 
-func part1(graph []string) int {
+var dirs = []Coord{
+	{-1, -1}, {-1, 0}, {-1, +1},
+	{0, -1}, {0, +1},
+	{+1, -1}, {+1, 0}, {+1, +1},
+}
+
+func inBounds(c Coord, m int, n int) bool {
+	return (0 <= c.R && c.R < m) && (0 <= c.C && c.C < n)
+}
+
+func part1(graph [][]rune) int {
 	m, n := len(graph), len(graph[0])
 	ans := 0
 	for i := range m {
 		for j := range n {
-			if graph[i][j] == '.' {
+			if graph[i][j] != '@' {
 				continue
 			}
-			cur := (getVal(i-1, j-1, graph) + getVal(i-1, j, graph) + getVal(i-1, j+1, graph) +
-				getVal(i, j-1, graph) + getVal(i, j+1, graph) +
-				getVal(i+1, j-1, graph) + getVal(i+1, j, graph) + getVal(i+1, j+1, graph))
+			cur := 0
+			for _, d := range dirs {
+				c := Coord{i + d.R, j + d.C}
+				if inBounds(c, m, n) && (graph[c.R][c.C] == '@') {
+					cur += 1
+				}
+			}
 			if cur < 4 {
 				ans++
+			}
+		}
+	}
+	return ans
+}
+
+func part2(graph [][]rune) int {
+	m, n := len(graph), len(graph[0])
+	neiCount := make([][]int, m)
+	for i := range m {
+		neiCount[i] = make([]int, n)
+	}
+	for i := range m {
+		for j := range n {
+			if graph[i][j] != '@' {
+				continue
+			}
+			for _, d := range dirs {
+				c := Coord{i + d.R, j + d.C}
+				if inBounds(c, m, n) && (graph[c.R][c.C] == '@') {
+					neiCount[c.R][c.C]++
+				}
+			}
+		}
+	}
+
+	q := dll.New()
+	for i := range m {
+		for j := range n {
+			if graph[i][j] == '@' && neiCount[i][j] < 4 {
+				q.Add(Coord{i, j})
+				graph[i][j] = '.'
+			}
+		}
+	}
+
+	ans := 0
+	for q.Size() > 0 {
+		ci, _ := q.Get(0)
+		c := ci.(Coord)
+		q.Remove(0)
+		ans++
+		for _, d := range dirs {
+			nc := Coord{c.R + d.R, c.C + d.C}
+			if inBounds(nc, m, n) && (graph[nc.R][nc.C] == '@') {
+				neiCount[nc.R][nc.C]--
+				if neiCount[nc.R][nc.C] < 4 {
+					q.Add(nc)
+					graph[nc.R][nc.C] = '.'
+				}
 			}
 		}
 	}
@@ -46,11 +105,12 @@ func main() {
 	defer inp.Close()
 
 	scanner := bufio.NewScanner(inp)
-	graph := make([]string, 0)
+	graph := make([][]rune, 0)
 	for scanner.Scan() {
-		line := scanner.Text()
+		line := []rune(scanner.Text())
 		graph = append(graph, line)
 	}
 
-	fmt.Println(part1(graph))
+	// fmt.Println(part1(graph))
+	fmt.Println(part2(graph))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ChakradharG/AdventOfCode
 
 go 1.24.1
+
+require github.com/emirpasic/gods v1.18.1 // indirect
