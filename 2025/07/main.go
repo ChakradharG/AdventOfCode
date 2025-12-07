955199c694e3f5dc8acf393a222cf86cc10ff13d
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2025-12-07 14:51:30 +0530

    day07-part2

--- 2025/07/main.go
+++ 2025/07/main.go
@@ -24,6 +24,25 @@ func part1(scanner *bufio.Scanner) int {
 	return ans
 }
 
+func part2(scanner *bufio.Scanner) int {
+	beam := map[int]int{}
+	ans := 0
+	for scanner.Scan() {
+		for j, c := range scanner.Text() {
+			if c == 'S' {
+				ans++
+				beam[j]++
+			} else if (c == '^') && (beam[j] > 0) {
+				beam[j-1] += beam[j]
+				beam[j+1] += beam[j]
+				ans += beam[j]
+				beam[j] = 0
+			}
+		}
+	}
+	return ans
+}
+
 func main() {
 	inp, err := os.Open("./input.txt")
 	if err != nil {
@@ -33,5 +52,6 @@ func main() {
 
 	scanner := bufio.NewScanner(inp)
 
-	fmt.Println(part1(scanner))
+	// fmt.Println(part1(scanner))
+	fmt.Println(part2(scanner))
 }
