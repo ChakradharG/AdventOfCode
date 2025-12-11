ebe8946e4925b34300b35ba6225e3482006345fe
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2025-12-11 20:32:23 +0530

    11-1

---
+++ 2025/11/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+var (
+	adj  map[string][]string
+	memo map[string]uint64
+)
+
+func dfs(src string, dst string) uint64 {
+	if src == dst {
+		return uint64(1)
+	}
+	key := src + dst
+	_, ok := memo[key]
+	if !ok {
+		for _, v := range adj[src] {
+			memo[key] += dfs(v, dst)
+		}
+	}
+	return memo[key]
+}
+
+func part1() uint64 {
+	memo = make(map[string]uint64)
+	return dfs("you", "out")
+}
+
+func main() {
+	inp, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer inp.Close()
+
+	scanner := bufio.NewScanner(inp)
+	adj = make(map[string][]string)
+
+	for scanner.Scan() {
+		parts := strings.Split(scanner.Text(), ": ")
+		u := parts[0]
+		for v := range strings.SplitSeq(parts[1], " ") {
+			adj[u] = append(adj[u], v)
+		}
+	}
+
+	fmt.Println(part1())
+}
