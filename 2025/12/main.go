3eeeded34049151540c60ba7e75db050b48e1e3e
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2025-12-13 00:20:48 +0530

    12

---
+++ 2025/12/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func part1(scanner *bufio.Scanner) uint64 {
+	ans := uint64(0)
+	for scanner.Scan() {
+		parts := strings.Split(scanner.Text(), ": ")
+		dims := strings.Split(parts[0], "x")
+		l, _ := strconv.Atoi(dims[0])
+		w, _ := strconv.Atoi(dims[1])
+		cnt := 0
+		for c := range strings.SplitSeq(parts[1], " ") {
+			x, _ := strconv.Atoi(c)
+			cnt += x
+		}
+		if cnt <= (l/3)*(w/3) {
+			ans++
+		}
+	}
+	return ans
+}
+
+func main() {
+	inp, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer inp.Close()
+
+	scanner := bufio.NewScanner(inp)
+	for range 30 {
+		// the shapes are not relevant
+		scanner.Scan()
+	}
+
+	fmt.Println(part1(scanner))
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ChakradharG/AdventOfCode
 
 go 1.24.1
 
-require github.com/emirpasic/gods v1.18.1 // indirect
+require github.com/emirpasic/gods v1.18.1
