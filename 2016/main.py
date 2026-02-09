f41c600711cdd76e5ca46235bbc55b694ff01410
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2026-02-09 20:15:52 +0530

    ..

--- 2016/main.py
+++ 2016/main.py
@@ -3,10 +3,12 @@ import importlib
 
 def main() -> None:
 	try:
-		importlib.import_module(f"day{sys.argv[1]}.solution").run()
+		solution = importlib.import_module(f"day{sys.argv[1]}.solution")
 	except IndexError:
 		print("Please provide a day to run (e.g., '01', '02').")
 	except ModuleNotFoundError:
 		print(f"Module day{sys.argv[1]} not found.")
+	else:
+		solution.run()
 
 main()
